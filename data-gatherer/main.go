f76ae1a393f2d4638bec9deeea3baf28be745077
Burg,Krijn K.R. van der <k.vanderburg@student.fontys.nl>
2017-10-01 01:35:49 +0200

    Reviewed data-gatherer main code and added TODO items; Added code comments to data-gatherer main : please make these yourself from now on!;

--- data-gatherer/main.go
+++ data-gatherer/main.go
@@ -1,65 +1,82 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"time"
 	"io/ioutil"
+	"encoding/json"
 
-	"golang.org/x/oauth2"
-	"log"
 	"DataWall/cassandra"
 	"DataWall/config"
+
+	"golang.org/x/oauth2"            // Authentication library
+	log "github.com/sirupsen/logrus" // Logging library
 )
 
-const devicesEndpointUrl string = "https://api.fhict.nl/location/devices"
-const interval time.Duration = 20000 * time.Millisecond
+//TODO Move to JSON config file! And build endpoint string down to several domain & protocol levels!
+const devicesEndpointUrl string = "https://api.fhict.nl/location/devices" // Fontys endpoint url
+const interval time.Duration = 20000 * time.Millisecond                   // Time with 20 seconds interval
 
-var logging bool
-var token string
-
+//TODO
+/** Data-gatherer main
+ *
+ */
 func main() {
-	fmt.Println("Application started...")
+	log.Info("Starting data-gatherer application!")
 
-	cfg := *config.Get()
-	logging = cfg.Logging
-	token = cfg.Token
-
-	// Start the function call loop
+	// call getDataFromApi func every tick predefined by interval var.
 	doEvery(interval, getDataFromApi)
 }
 
+/** do Every //TODO Func name not clear enough
+ * Timer to repeat func every given amount of time. //TODO Does this have to be a seperate func? Can it not be recursive?
+ * @param interval in whole seconds.
+ 8 @param function name to repeat every interval tick
+ */
 func doEvery(interval time.Duration, repeatFunction func(time.Time)) {
 	for currentTime := range time.Tick(interval) {
 		repeatFunction(currentTime)
 	}
 }
 
+/** getDataFromApi
+ * Get Fontys authentication token. Connect to devices location endpoint, read & serialize response.
+ * currenTime //TODO Unused parameter? NO!
+ */
 func getDataFromApi(currentTime time.Time) {
-	if logging {
-		fmt.Println("Start:", currentTime)
+	log.WithFields(log.Fields{
+		"Start time": time.Now(),
+	}).Debug("Retrieving data from Fontys API")
+
+	// TODO Should this variable be predefined?
+	var devices []cassandra.Device
+
+	// TODO Comment incomplete, elaborate!
+	// Set tokenSource for OAuth?
+	tokenSource := &TokenSource{
+		AccessToken: config.Get().Token,
 	}
 
-	// Get a response from the Fontys API.
-	tokenSource := &TokenSource{
-		AccessToken: token,
-	}
+	// TODO DEPRECATED! NO!
 	resp, _ := oauth2.NewClient(oauth2.NoContext, tokenSource).Get(devicesEndpointUrl)
 	body, _ := ioutil.ReadAll(resp.Body)
 	defer resp.Body.Close()
+
+	// TODO Why not directly use body string in the unmarshal?
 	jsonData := string(body)
 
-	// Convert the JSON to a struct array.
-	var devices []cassandra.Device
+	// Serialize JSON response to device struct.
 	err := json.Unmarshal([]byte(jsonData), &devices)
 	if err != nil {
-		log.Panic("Could not convert json data to struct. Data from Fontys API:", jsonData)
+		// TODO Handle error more gracefully!
+		log.WithFields(log.Fields{
+			"End time": err.Error(),
+		}).Error("Could not serialize JSON response to device struct")
 	}
 
-	// Store the data in the database.
+	// Send devices list to insert func to be inserted into the DB.
 	cassandra.InsertDevices(devices)
 
-	if logging {
-		fmt.Println("End:", time.Now())
-	}
+	log.WithFields(log.Fields{
+		"End time": time.Now(),
+	}).Debug("Finished retrieving data from Fontys API")
 }
