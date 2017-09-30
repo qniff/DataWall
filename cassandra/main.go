c93c8ae065a528a1aa7adce3ba3f4859905eacf0
Burg,Krijn K.R. van der <k.vanderburg@student.fontys.nl>
2017-09-30 23:59:35 +0200

    Restored comments in config; Added comments to cassandra main;

--- cassandra/main.go
+++ cassandra/main.go
@@ -6,20 +6,25 @@ import (
 	"log"
 )
 
-// Retrieve data from the database.
-// limit: The amount of rows you want to limit your request to.
+/** GetData
+ * Requests devices location from database with given maximum record limit.
+ * @param limit: Amount of rows you want to limit your request to. Must be positive.
+ * @return List of device locations by struct Locations.
+ */
 func GetDevices(limit int) []Device {
-	var locList []Device
-	m := map[string]interface{}{}
+	var locList []Device          // Predeclared list of Device struct.
+	m := map[string]interface{}{} // What is this?
 
-	// Create the query.
-	query := fmt.Sprintf(
+	// Formatting database query. Selecting fields user_hash, createdAt, loc_x, loc_y, loc_y with maximum records given by limit variable.
+	// TODO Protect SQL injection
+	selectLocationsQuery := fmt.Sprintf(
 		"%s %d",
 		"SELECT user_hash, createdat, loc_x, loc_y, loc_z FROM locations LIMIT ",
 		limit)
-	iterable := session().Query(query).Iter()
+	// Create and assign variable of query iterator
+	iterable := session().Query(selectLocationsQuery).Iter()
 
-	// Iterate over results.
+	// Iterate over DB query result and add to map.
 	for iterable.MapScan(m) {
 		locList = append(locList, Device{
 			Hash:      m["user_hash"].(string),
@@ -30,19 +35,28 @@ func GetDevices(limit int) []Device {
 		})
 		m = map[string]interface{}{}
 	}
-
 	return locList
 }
 
+/** InsertDevices
+ * Creates new goroutine for each device in Device list to insert each device into database
+ * @param devices. List of all serialized devices to insert.
+ */
 func InsertDevices(devices []Device) {
-	for _, device := range devices  {
+	for _, device := range devices {
+		// Create new Goroutine while calling insert function.
 		go insert(device)
 	}
 }
 
+/** InsertDevices
+ * Inserts struct parameter into database
+ * @param device. Struct of device must have all fields populated or null will be inserted.
+ */
 func insert(device Device) {
-	if err := session().Query(`INSERT INTO locations (loc_x, loc_y, loc_z, user_hash, createdat) VALUES (?, ?, ?, ?, ?)`,
-		device.X, device.Y, device.Z, device.Hash, time.Now()).Exec(); err != nil {
+	// Insert device struct into database
+	if err := session().Query(`INSERT INTO locations (loc_x, loc_y, loc_z, user_hash, createdat) VALUES (?, ?, ?, ?, ?)`, device.X, device.Y, device.Z, device.Hash, time.Now()).Exec(); err != nil {
+		//
 		log.Fatal(err)
 	}
 }
--- config/config.go
+++ config/config.go
@@ -1,36 +1,51 @@
 package config
 
 import (
-	"sync"
-	"fmt"
-	"encoding/json"
-	"os"
-	"path/filepath"
+	"sync"          // Config get() func only required to run once
+	"encoding/json" // Serializing JSON configuration file to struct
+	"os"            // Open configuration file
+	"path/filepath" // Define config file path
+
+	log "github.com/sirupsen/logrus" // Logging errors
 )
 
 // Struct to store all global variables.
 type Configuration struct {
-	IpAddress string
-	Keyspace  string
-	ApiPort   int
-	Logging   bool
-	Token     string
+	IpAddress string // IP addresses of cassandra database
+	Keyspace  string // Keyspace (DB name) of cassandra database
+	ApiPort   uint8  // The port on which the API will run. Always positive and below 65535
+	Logging   bool   // Whether to display logs or not
+	Token     string // Auth token from Fontys API
 }
 
-var conf *Configuration
-var once sync.Once
+var conf *Configuration // Predeclared global configuration struct, accessible by Get() func returning conf pointer.
 
-// Return the configuration.
+/** Config.Get
+ * Opens json config file. Decodes content to json into var conf of Configuration struct.
+ * @return Configuration pointer
+ */
 func Get() *Configuration {
-	once.Do(func() {
+	sync.Once.Do(func() {
+		// Set file path to be serialized
 		absPath, _ := filepath.Abs("../config/config.json")
-		file, err := os.Open(absPath)
+
+		// All logs by contextLogger now include file path
+		contextLogger := log.WithFields(log.Fields{
+			"path": absPath,
+		})
+		contextLogger.Info("Retrieving and setting configuration from file...")
+		file, fileErr := os.Open(absPath)
 		defer file.Close()
-		if err != nil {
-			fmt.Println(err.Error())
+		if fileErr != nil {
+			log.WithFields(log.Fields{
+				"Error": fileErr.Error(),
+			}).Fatal("Failed opening file! ")
 		}
+
+		// Decoding (serializing) JSON content of file to Configuration struct
 		jsonParser := json.NewDecoder(file)
 		jsonParser.Decode(&conf)
+		contextLogger.Info("Configuration initialized!")
 	})
 	return conf
 }
