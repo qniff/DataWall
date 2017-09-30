081218af1066fcaa2cabcef8c5e175d3b7d9a0c9
Burg,Krijn K.R. van der <k.vanderburg@student.fontys.nl>
2017-10-01 01:02:15 +0200

    Added code comments to api controller;

--- api/controller/controller.go
+++ api/controller/controller.go
@@ -4,12 +4,17 @@ import (
 	"net/http"
 	"strconv"
 	"path/filepath"
+
+	log "github.com/sirupsen/logrus" // Logging library
 )
 
-const defaultLimit = 50
-const headerLimit = "Limit"
-const defaultResponse = "{}"
+const defaultLimit int = 50         // Default limit if none could be found in header.
+const headerLimit string = "Limit"  // Field to search for in header.
+const defaultResponse string = "{}" // Empty response body predefined.
 
+/** RegisterEndPoints
+ * Set all endpoints of webserver to listen and serve to.
+ */
 func RegisterEndPoints() {
 	// GET: /
 	http.HandleFunc("/", root)
@@ -17,20 +22,38 @@ func RegisterEndPoints() {
 	http.HandleFunc("/data", getAllLocations)
 }
 
-// Return the value supplied by the "Limit" header.
-// If the value is not an integer return the default value.
-func getLimit(h *http.Header) int {
-	tempLimit := h.Get(headerLimit)
-	limit, err := strconv.Atoi(tempLimit)
+/** getLimit
+ * Gets header limit from HTTP requests and returns this limit. If no limit header cannot be retrieved it returns programmatically default set limit.
+ * @param h:
+ * @return limit : integer of maximum amount of records to return
+ */
+func getLimit(header *http.Header) int {
+	// Get header limit from the HTTP request
+	headerLimit := header.Get(headerLimit)
+	log.WithFields(log.Fields{
+		"limit": headerLimit,
+	}).Debug("Header limit of HTTP request")
+	limit, err := strconv.Atoi(headerLimit)
 	if err != nil {
+		log.WithFields(log.Fields{
+			"Error": err.Error(),
+		}).Error("Could not retrieve header limit from HTTP request!")
+		// Return default set limit if none could be retrieved from header.
 		return defaultLimit
 	}
 	return limit
 }
 
-// The default root which serves the static file with the endpoints.
+/** root
+ * Serves static HTML file displaying endpoints by default.
+ * @param writer
+ * @param request
+ */
 func root(writer http.ResponseWriter, request *http.Request) {
+	// Set header of request
 	writer.Header().Set("Content-Type", "text/html; charset=utf-8")
+	// Get file path of static html file to serve as root.
 	absPath, _ := filepath.Abs("../api/static/home.html")
+	// Serve (return) static html file to request
 	http.ServeFile(writer, request, absPath)
 }
