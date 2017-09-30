f02eca2243fa375752ef8e3f0e4eaf1101d0db5a
Burg,Krijn K.R. van der <k.vanderburg@student.fontys.nl>
2017-10-01 01:10:44 +0200

    Added code comments to api locationHandler;

--- api/controller/locationHandler.go
+++ api/controller/locationHandler.go
@@ -1,21 +1,37 @@
 package controller
 
 import (
-	"DataWall/cassandra"
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"DataWall/cassandra"
+
+	log "github.com/sirupsen/logrus" // Logging library
 )
 
 func getAllLocations(writer http.ResponseWriter, request *http.Request) {
+	log.Debug("Fetching and attempting to serve all devices!")
+	// Get limit for maximum records to fetch
 	limit := getLimit(&request.Header)
 
+	// Fetch list of all devices from database with given limit
 	data := cassandra.GetDevices(limit)
 
-	b, err := json.Marshal(data)
-	if err != nil {
+	// Serialize list to JSON format
+	b, serializeErr := json.Marshal(data)
+
+	// Handle possible error
+	if serializeErr != nil {
+		log.WithFields(log.Fields{
+			"Error": serializeErr,
+		}).Error("Failed to serialize devices list to JSON!")
+		// Write error to webpage
+		// TODO writing error to user is potentially dangerous!
 		fmt.Fprint(writer, defaultResponse)
 	} else {
+		// Write devices endpoint on webpage
 		fmt.Fprintf(writer, string(b))
+		log.Debug("Successfully fetched devices list and printed on screen!")
 	}
 }
